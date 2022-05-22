104ff85e2e40f06553af323319df20bd77169ea3
gemher <gema.334@gmail.com>
2022-05-22 00:05:55 -0500

    Added bootstrap and search engine

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>Definition</p>
+        <p>Definition search</p>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <a
+          href="https://github.com/gemher/dictionary-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source
+        </a>{" "}
+        code by Gema Hernandez
+      </footer>
     </div>
   );
 }
---
+++ src/Dictionary.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+export default function Dictionary() {
+  const [city, setCity] = useState("");
+  const [message, setMessage] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setMessage(`It is currently 70ºF in ${city}`);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div className="Search">
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder={"City"} onChange={changeCity} />
+        <input type="submit" class="btn btn-dark" value="Search" />
+      </form>
+      <h2>{message}</h2>
+    </div>
+  );
+}
