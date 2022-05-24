a1e4e8786fba99d4234e3fcd174aeb6abd6587ff
gemher <gema.334@gmail.com>
2022-05-24 02:38:59 -0500

    Fixed js

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -2,12 +2,13 @@ import React from "react";
 
 export default function Synonyms(props) {
   if (props.synonyms) {
-    return;
-    <ul>
-      {props.synonyms.map(function (synonym, index) {
-        return <li key={index}>${synonym}</li>;
-      })}
-    </ul>;
+    return (
+      <ul>
+        {props.synonyms.map(function (synonym, index) {
+          return <li key={index}>${synonym}</li>;
+        })}
+      </ul>
+    );
   } else {
     return null;
   }
