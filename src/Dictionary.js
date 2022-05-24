5b550c10f9a03f1e251619c4852009f76eee800d
gemher <gema.334@gmail.com>
2022-05-24 01:00:51 -0500

    Dictionary api integration

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
@@ -6,8 +7,10 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword} definition`);
   }
+
+  let apiUrl = "https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}";
+  axios.get(apiUrl).then(handleResponse);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
