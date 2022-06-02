307287d64c68d2101ecac1558df7702f83b8d47a
gemher <gema.334@gmail.com>
2022-06-01 19:46:11 -0500

    Addded phonetic component

--- src/Meaning.js
+++ src/Meaning.js
@@ -12,6 +12,7 @@ export default function Meaning(props) {
             <p>
               {definition.definition}
               <br />
+              <strong>Example: </strong>
               <em>{definition.example}</em>
               <br />
               <Synonyms synonyms={definition.synonyms} />
